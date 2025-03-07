77ef00273879cd3b124084dedf70da4390e929f5
TrungHiu2610 <hieult2610@gmail.com>
2025-03-07 22:35:10 +0700

    Layout customer, ViewComponent(Menu dong - partialview), danh muc hang hoa, trang hien thi san pham, xem toi part 4 - 11p44s

---
+++ ECommerceMVC/Controllers/HangHoaController.cs
@@ -0,0 +1,33 @@
+﻿using ECommerceMVC.Data;
+using ECommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceMVC.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        private readonly Hshop2023Context db;
+
+        public HangHoaController(Hshop2023Context context) => db = context;
+        public IActionResult Index(int? maLoai)
+        {
+            var hangHoas = db.HangHoas.AsQueryable();
+
+            if(maLoai.HasValue)
+            {
+                hangHoas = hangHoas.Where(h => h.MaLoai == maLoai);
+            }
+
+            var data = hangHoas.Select(d => new HangHoaVM
+            {
+                MaHH = d.MaHh,
+                TenHH = d.TenHh,
+                HinhAnh = d.Hinh ?? "",
+                DonGia = d.DonGia ?? 0,
+                MoTaNgan = d.MoTaDonVi ?? "",
+                TenLoai = d.MaLoaiNavigation.TenLoai
+            });
+            return View(data);
+        }
+    }
+}
---
+++ ECommerceMVC/ViewComponents/MenuLoaiViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using ECommerceMVC.Data;
+using ECommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+public class MenuLoaiViewComponent : ViewComponent
+{
+    private readonly Hshop2023Context db;
+
+    public MenuLoaiViewComponent(Hshop2023Context context) => db = context;
+
+    public IViewComponentResult Invoke()
+    {
+        var data = db.Loais.Select(loai => new MenuLoaiVM
+        {
+            MaLoai = loai.MaLoai,
+            TenLoai = loai.TenLoai,
+            SoLuong = loai.HangHoas.Count
+        });
+        return View("Default", data);
+    }
+}
