73d2851ac04c799df632f45e5d0a0fa1deeb1219
TrungHiu2610 <hieult2610@gmail.com>
2025-03-10 14:17:31 +0700

    chi tiet hang hoa, toi vid gio hang (dùng Component.InvokeAsync như 1 partial view động)

--- ECommerceMVC/Controllers/HangHoaController.cs
+++ ECommerceMVC/Controllers/HangHoaController.cs
@@ -47,9 +47,46 @@ namespace ECommerceMVC.Controllers
                 DonGia = d.DonGia ?? 0,
                 MoTaNgan = d.MoTaDonVi ?? "",
                 TenLoai = d.MaLoaiNavigation.TenLoai
-            }); 
+            });
 
             return View(data);
+        }
+
+        public IActionResult Detail(int maHH)
+        {
+            var hangHoas = db.HangHoas.AsQueryable();
+
+            HangHoa? product = hangHoas.FirstOrDefault(p => p.MaHh == maHH);
+
+            // hàng hóa cùng loại
+            List<HangHoaVM> lstRelatedProducts = new List<HangHoaVM>();
+            if (product != null)
+            {
+                lstRelatedProducts = hangHoas.Where(d => d.MaHh != maHH && d.MaLoai == product.MaLoai)
+                                             .Select(d => new HangHoaVM
+                                             {
+                                                 MaHH = d.MaHh,
+                                                 TenHH = d.TenHh,
+                                                 HinhAnh = d.Hinh ?? "",
+                                                 DonGia = d.DonGia ?? 0,
+                                                 MoTaNgan = d.MoTaDonVi ?? "",
+                                                 TenLoai = d.MaLoaiNavigation.TenLoai
+                                             }).Take(4).ToList();
+
+                HangHoaVM product_to_find = new HangHoaVM()
+                {
+                    MaHH = product.MaHh,
+                    TenHH = product.TenHh,
+                    HinhAnh = product.Hinh ?? "avatar.jpg",
+                    TenLoai = (product.MaLoaiNavigation != null) ? product.MaLoaiNavigation.TenLoai : "Default",
+                    MoTaNgan = product.MoTa ?? "No description",
+                    DonGia = product.DonGia ?? 0
+                };
+                ViewBag.lstRelatedProducts = lstRelatedProducts;
+                return View(product_to_find);
+            }
+            TempData["Message"] = $"The product's id {maHH} is not avaiable";
+            return Redirect("/404");
         }
     }
 }
--- ECommerceMVC/ViewModels/HangHoaVM.cs
+++ ECommerceMVC/ViewModels/HangHoaVM.cs
@@ -5,10 +5,10 @@ namespace ECommerceMVC.ViewModels
     public class HangHoaVM
     {
         public int MaHH { get; set; }
-        public string TenHH { get; set; }
-        public string HinhAnh { get; set; }
+        public string? TenHH { get; set; }
+        public string? HinhAnh { get; set; }
         public double DonGia { get; set; }
-        public string MoTaNgan { get; set; }
-        public string TenLoai { get; set; }
+        public string? MoTaNgan { get; set; }
+        public string? TenLoai { get; set; }
     }
 }
